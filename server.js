acace586f69cb2fc192600385b75d794d3d7cf93
Dhruv Patel <dhruvpatel.aj@gmail.com>
2019-12-10 00:22:51 -0700

    obtain messages from backend
    conversion of jquery from slim to normal
    update of index html to present html

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+
+//* load up express
 var express = require('express')
+
+//* store express calls
 var app = express()
 
-
+//* use the html page
 app.use(express.static(__dirname))
+
+//place holder messages lists
+var messages = [
+    {name: 'Timon', message: 'Sista'},
+    {name: 'Pumba', message: 'Brudda'}
+
+]
+
+/**  
+ * *create a new html page: /message
+ * allows for requests and responses.
+*/
+app.get('/messages', (req, res) => {
+    res.send(messages);
+})
 
 //* the port it is listening on
 app.listen(3000, () => {
